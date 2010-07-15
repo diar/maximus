43f38c74e30190380596058e3fbffb3ba30fa472
valmon <valmon@xakep.ru>
2010-07-15 17:54:44 +0400

    add typograf

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ActiveRecord::Base
   validates_presence_of :uri, :title
+  validates_uniqueness_of :uri
+  typograf :text, :use_br => false
   has_attached_file :photo,
     :styles => {
     :big => "1024x1024>",
